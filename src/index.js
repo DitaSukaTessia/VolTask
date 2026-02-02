dfeeda7b6878a27b174d1bea67e7896e8a1fc6cc
AV_lab <atashitachi434@gmail.com>
2026-02-02 20:48:20 +0700

    mounting routes.js to server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import express from "express";
-import { createTaskController } from "./controllers/createTaskController.js";
-import { getTaskByIdController } from "./controllers/getTaskController.js";
-import { getAllTasksController } from "./controllers/getTaskController.js";
-import { updateTaskController } from "./controllers/updateTaskController.js";
-import { deleteTaskController } from "./controllers/deleteTaskController.js";
+import router from "./routes/taskRoutes.js";
 
 const server = express();
 server.use(express.json());
 
-server.post("/tasks", createTaskController);
-server.get("/tasks", getAllTasksController);
-server.get("/tasks/:id", getTaskByIdController);
-server.put("/tasks/:id", updateTaskController);
-server.delete("/tasks/:id", deleteTaskController);
+server.use("/api", router);
 
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000/");
